9724a31e20ce9b0f995b3a7b17ff39afa444b9dc
albert18 <caps18@hotmail.com>
2018-12-14 21:33:37 -0500

    some commit

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
 "use strict";
 // console.log("test 123");
-//string
+///////////////////
+//Assigning Type// 
+//note: 
+///////////////////
+///////////////////
+//string//
+//note: In ts you can't overwrite to interger if it's string, not like in regular javascript.
+var myName = 'Max';
+// myName = 28;
+///////////////////
+///////////////////
+//Numbers and Boolean// 
+//note: for numbers vice and versa like string
+var hasHobbies = false;
+// hasHobbies = 1;
+///////////////////
+///////////////////
+//Assigning Type// 
+//note: can't not do string 
+var myRealAge;
+myRealAge = 27;
+// myRealAge = '27'; 
+///////////////////
+///////////////////
+//Array and Types// 
+//note: Usy anyy[] to hold strings and intergers
+var cooking = ["fries", "sports"];
+cooking = [100];
+// console.log(cooking[0]);
+///////////////////
+///////////////////
+//Tuples// 
+//note: 
+var address = ["Super Street", 99];
+///////////////////
+///////////////////
+//Enums// 
+//note: You can assign value, for ex. add 100 to Green and value will be 100
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
+///////////////////
+///////////////////
+//Assigning Type// 
+//note: 
+///////////////////
+///////////////////
+//Assigning Type// 
+//note: 
+///////////////////
--- app.ts
+++ app.ts
@@ -1,4 +1,63 @@
 // console.log("test 123");
+///////////////////
+//Assigning Type// 
+//note: 
+///////////////////
 
+///////////////////
+//string//
+//note: In ts you can't overwrite to interger if it's string, not like in regular javascript.
+let myName = 'Max';
+// myName = 28;
+///////////////////
 
-//string
+///////////////////
+//Numbers and Boolean// 
+//note: for numbers vice and versa like string
+let hasHobbies = false;
+// hasHobbies = 1;
+///////////////////
+
+///////////////////
+//Assigning Type// 
+//note: can't not do string 
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = '27'; 
+///////////////////
+
+///////////////////
+//Array and Types// 
+//note: Usy anyy[] to hold strings and intergers
+let cooking: any[] = ["fries", "sports"];
+cooking = [100];
+// console.log(cooking[0]);
+///////////////////
+
+///////////////////
+//Tuples// 
+//note: 
+let address: [string, number] = ["Super Street", 99];
+///////////////////
+
+///////////////////
+//Enums// 
+//note: You can assign value, for ex. add 100 to Green and value will be 100
+enum Color {
+    Gray,
+    Green = 100,
+    Blue
+}
+let myColor: Color = Color.Green
+console.log(myColor);
+///////////////////
+
+///////////////////
+//Any type// 
+//note: 
+///////////////////
+
+///////////////////
+//Assigning Type// 
+//note: 
+///////////////////
