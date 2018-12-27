ba4b88ca6633504f4e0c97011bff1e842304a778
albert18 <caps18@hotmail.com>
2018-12-27 10:22:06 -0500

    update

---
+++ math-section6/circle.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PI = 3.14;
+function calculateCirumcumference(diameter) {
+    return diameter * exports.PI;
+}
